d382e312ac2a1a5c069f1c8c9b63dc230a5067a9
Thomas Hall Jr <thomashalljunior@gmail.com>
2021-11-24 00:09:04 -0500

    Refactor bottles.rb

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,34 @@
 class Bottles
   def verse(num_of_bottles)
-    "#{num_of_bottles == 0 ? "No more" : num_of_bottles} bottle#{"s" if num_of_bottles > 1 or num_of_bottles == 0} of beer on the wall, " +
-    "#{num_of_bottles == 0 ? "no more" : num_of_bottles} bottle#{"s" if num_of_bottles > 1 or num_of_bottles == 0} of beer.\n" +
-    "#{num_of_bottles == 0 ? "Go to the store and buy some more, " : "Take #{num_of_bottles-1 == 0 ? "it" : "one"} down and pass it around, "}" +
-    "#{if num_of_bottles-1 == 0; "no more" elsif num_of_bottles-1 < 0; "99" else num_of_bottles-1 end;} bottle#{"s" if num_of_bottles-1 > 1 or num_of_bottles-1 == 0 or num_of_bottles-1 < 0} of beer on the wall.\n"
+    bottles_left = num_of_bottles - 1
+
+    bottles_plural = ""
+    bottles_plural = "s" if num_of_bottles != 1
+
+    bottles_left_plural = ""
+    bottles_left_plural = "s" if bottles_left != 1
+
+    if num_of_bottles == 0
+      how_many_bottles = "no more"
+      next_steps = "Go to the store and buy some more, "
+    else
+      how_many_bottles = num_of_bottles.to_s
+      next_steps = "Take #{bottles_left == 0 ? "it" : "one"} down and pass it around, "
+    end
+
+    how_many_bottles_left =
+    case bottles_left
+    when 0
+      "no more"
+    when -Float::INFINITY..-1
+      "99"
+    else
+      bottles_left
+    end
+
+    "#{how_many_bottles.capitalize} bottle#{bottles_plural} of beer on the wall, " +
+    "#{how_many_bottles} bottle#{bottles_plural} of beer.\n" +
+    next_steps +
+    "#{how_many_bottles_left} bottle#{bottles_left_plural} of beer on the wall.\n"
   end
 end
